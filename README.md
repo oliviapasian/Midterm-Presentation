f9fa0f94f79fe15975c06246f5146d380896b373
oliviapasian <99832146+oliviapasian@users.noreply.github.com>
2023-10-23 23:57:02 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Here are the sources I used for this project:
+
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_full_page_tabs
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_lightbox
+https://www.w3schools.com/howto/howto_js_accordion.asp
+https://www.w3schools.com/howto/howto_js_collapsible.asp
+https://codepen.io/falldowngoboone/pen/PwzPYv
---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+//  Tabs
+function openPage(pageName, elmnt, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
+
+// Intro overlay
+$('.get-started').on('click', function () {
+  $('.conclusionpage').fadeOut("fast");
+  $('.introduction').fadeOut("slow");
+});
+
+$('.conclusion').on('click', function () {
+  $('.conclusionpage').fadeIn();
+});
+
+// Image Lightbox
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
